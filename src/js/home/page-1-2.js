7d1db3956cbd4811cefc8e2e339a4576fac14045
lynchuh <lynchuh@gmail.com>
2018-07-20 15:05:19 +0800

    将model上的数据渲染到页面上

--- src/js/home/page-1-2.js
+++ src/js/home/page-1-2.js
@@ -1,35 +1,66 @@
-console.log('我是module2')
 {
-  let view={
-    el:'section.songs',
-    init(){
-      this.$el=$(this.el)
+  let view = {
+    el: 'section.songs',
+    init() {
+      this.$el = $(this.el)
+    },
+    template: `
+    <li>
+    <h3>{{song.name}}</h3>
+    <p>
+      <svg class="icon icon-sq">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-sq"></use>
+      </svg>
+      {{song.singer}}
+    </p>
+    <a class="playButton" href="./song.html?id={{song.id}}">
+      <svg class="icon icon-play">
+        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
+      </svg>
+    </a>
+  </li>`,
+    render(data) {
+      let {songs}=data
+      songs.map((song)=>{
+        let li= this.template
+          .replace('{{song.name}}',song.name)
+          .replace('{{song.singer}}',song.singer)
+          .replace('{{song.id}}',song.id)
+        this.$el.append(li)
+        console.log(li)
+      })
     }
   }
-  let model={
-    data:{
-      songs:[]
+  let model = {
+    data: {
+      songs: []
     },
-    fetch(){
+    fetch() {
       var query = new AV.Query('Song')
-      return query.find().then((songs)=>{
-        this.data.songs=songs.map((song)=>{
-          return {id:song.id,...song.attributes}
+      return query.find().then((songs) => {
+        this.data.songs = songs.map((song) => {
+          return {
+            id: song.id,
+            ...song.attributes
+          }
         })
         return songs
       })
     }
   }
-  let controller={
-    init(view,model){
-      this.view=view
+  let controller = {
+    init(view, model) {
+      this.view = view
       this.view.init()
-      this.model=model
-      this.model.fetch().then(()=>{
-        console.log(this.model.data)
+      this.model = model
+      this.model.fetch().then(() => {
+        this.view.render(this.model.data)
       })
+
     },
-    bindEvent(){}
+    bindEvent() {}
   }
-  controller.init(view,model)
+  controller.init(view, model)
+  console.log('我是module2')
+
 }
