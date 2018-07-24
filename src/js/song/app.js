50862aef6f86abca6462880a8b6e1e3d48e03d3f
lynchuh <lynchuh@gmail.com>
2018-07-24 10:07:36 +0800

    完成页面布局后修改播放/暂停bug

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,85 +1,87 @@
 {
-  let view={
-    el:'#app',
-    template:`
+  let view = {
+    el: '#app',
+    template: `
       <audio src={{url}}></audio>
     `,
-    render(){
-      
+    render() {
+
     },
-    find(selector){
+    find(selector) {
       return $(this.el).find(selector)[0]
     },
-    uploadMusic(data){
-      let audio=this.find('audio')
-      audio.src=data.url
+    uploadMusic(data) {
+      let audio = this.find('audio')
+      audio.src = data.url
     },
-    play(){
-      let audio=this.find('audio')
-      audio.play()
-    },
-    pause(){
-      let audio=this.find('audio')
-      audio.pause()
+    toggleStatus(status) {
+      let audio = this.find('audio')
+      if (status) {
+        $(this.find('.disc-container')).addClass('active')
+        audio.play()
+      } else {
+        $(this.find('.disc-container')).removeClass('active')
+        audio.pause()
+      }
     }
   }
-  let model={
-    data:{
-      id:'',
-      name:'',
-      singer:'',
-      url:''
+  let model = {
+    data: {
+      song: {
+        id: '',
+        name: '',
+        singer: '',
+        url: ''
+      },
+      status: false,
+
     },
-    fetch(){
+    fetch() {
       var query = new AV.Query('Song')
       return query.get(this.data.id).then((song) => {
-        Object.assign(this.data,song.attributes)
+        Object.assign(this.data.song, song.attributes)
         return song
       })
     },
   }
-  let controller={
-    init(view,model){
-      this.view=view
-      this.model=model
+  let controller = {
+    init(view, model) {
+      this.view = view
+      this.model = model
       this.getSongId()
-      this.model.fetch().then((data)=>{
-        this.view.uploadMusic(this.model.data)
+      this.model.fetch().then((data) => {
+        this.view.uploadMusic(this.model.data.song)
       })
       this.bindEvent()
     },
-    bindEvent(){
-      let playBtn=this.view.find('.play')
-      let pauseBtn=this.view.find('.pause')
-      $(playBtn).on('click',(e)=>{
-        this.view.play()
+    bindEvent() {
+      $(this.view.el).on('click', '.icon-wrapper', (e) => {
+        e.preventDefault()
+        this.model.data.status = !this.model.data.status
+        console.log(this.model.data)
+        this.view.toggleStatus(this.model.data.status)
       })
-      $(pauseBtn).on('click',(e)=>{
-        this.view.pause()
+      
 
-      })
     },
-    getSongId(){
+    getSongId() {
       let search = window.location.search
-      if(search.indexOf('?')===0){
-        search=search.substring(1)
+      if (search.indexOf('?') === 0) {
+        search = search.substring(1)
       }
-      let array=search.split('&').filter((v=>v))
+      let array = search.split('&').filter((v => v))
       let id = ''
-      for(let i=0;i<array.length;i++){
-        let kv=array[i].split('=')
-        let key= kv[0]
-        let value= kv[1]
-        if(key==='id'){
-          id=value
+      for (let i = 0; i < array.length; i++) {
+        let kv = array[i].split('=')
+        let key = kv[0]
+        let value = kv[1]
+        if (key === 'id') {
+          id = value
           break
         }
       }
-      this.model.data.id=id 
+      this.model.data.id = id
     },
-    
-
-
   }
-  controller.init(view,model)
+  controller.init(view, model)
 }
